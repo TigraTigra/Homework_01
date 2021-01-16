e11bd6a1d6484d8a392e586cbe818b241f7d2273
TigraTigra <pavelremarque@gmail.com>
2021-01-16 11:26:39 +0300

    Initial commit

---
+++ variables.py
@@ -0,0 +1,23 @@
+# Поработайте с переменными, создайте несколько,
+# выведите на экран, запросите у пользователя
+# несколько чисел и строк и сохраните
+# в переменные, выведите на экран.
+
+variable_1 = 1
+print(variable_1)
+variable_2 = 2
+print(variable_1)
+variable_3 = 3
+print(variable_3)
+
+print(variable_1, variable_2,variable_3)
+
+number_1 = int(input('Введите число: '))
+number_2 = int(input('Введите второе число: '))
+number_3 = int(input('Введите третье число: '))
+number_4 = int(input('Введите четвертое число: '))
+string_1 = input('Введите строчку: ')
+string_2 = input('Введите вторую строчку: ')
+string_3 = input('Введите третью строчку: ')
+string_4 = input('Введите четвертую строчку: ')
+print(f"{number_1}, {number_2}, {number_3}, {number_4}, {string_1}, {string_2}, {string_3}, {string_4}")
